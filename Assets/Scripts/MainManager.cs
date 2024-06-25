f1a69ea5106dc32d1807d94277e796f896bd35a9
DTSIBO <tsibodimitris@gmail.com>
2024-06-25 14:36:40 +0300

    final
    working

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class MainManager : MonoBehaviour
 {
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
 
+    public Text BestScore;
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (NameManager.Instance != null)
+        {
+            BestScore.text = "Best Score: " + NameManager.Instance.PlayerName + " : " + NameManager.Instance.BestScore;
+        }
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -77,12 +82,18 @@ public class MainManager : MonoBehaviour
         m_GameOver = true;
         StartCoroutine(BackMenu());
         GameOverText.SetActive(true);
+        if (m_Points >= NameManager.Instance.BestScore)
+        {
+            NameManager.Instance.BestScore = m_Points;
+            NameManager.Instance.PlayerName = NameManager.Instance.NewPlayer;
+            BestScore.text = "Best Score: " + NameManager.Instance.PlayerName + " : " + NameManager.Instance.BestScore;
+            NameManager.Instance.SavePlayer();
+        }
+
         
-
     }
-
     IEnumerator BackMenu()
-	{
+    {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(1);
 
@@ -91,3 +102,8 @@ public class MainManager : MonoBehaviour
 
 
 }
+
+
+
+    
+
--- Assets/Scripts/MenuUi.cs
+++ Assets/Scripts/MenuUi.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 
@@ -10,8 +11,10 @@ public class MenuUi : MonoBehaviour
     public static MenuUi Instance;
 
     
+    
 
-    // Start is called before the first frame update
+    
+       // Start is called before the first frame update
     public void StartNew()
     {
         SceneManager.LoadScene(0);
